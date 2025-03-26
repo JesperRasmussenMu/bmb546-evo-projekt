82519bb4f67a13bb232f38a9c094e8af550e2fed
JesperRasmussenMu <jeras24@student.sdu.dk>
2025-03-26 13:48:55 +0100

    v.4

--- r code.R
+++ r code.R
@@ -102,14 +102,30 @@ primate_div<-primate_eye/primate_weight
 reptile_div<-reptile_eye/reptile_weight
 rodent_div<-rodent_eye/rodent_weight
 
-# Line - not yet used - want to make R values
-amph_r<-lm(amph_div~amph_eye)
-bird_r<-lm(bird_div~bird_eye)
-fish_r<-lm(fish_div~fish_eye)
-mammal_r<-lm(mammal_div~mammal_eye)
-primate_r<-lm(primate_div~primate_eye)
-reptile_r<-lm(reptile_div~reptile_eye)
-rodent_r<-lm(rodent_div~rodent_eye)
+# Line 
+amph_line<-lm(amph_div~amph_eye)
+bird_line<-lm(bird_div~bird_eye)
+fish_line<-lm(fish_div~fish_eye)
+mammal_line<-lm(mammal_div~mammal_eye)
+primate_line<-lm(primate_div~primate_eye)
+reptile_line<-lm(reptile_div~reptile_eye)
+rodent_line<-lm(rodent_div~rodent_eye)
+
+# making R values
+bird_r<-round(cor(bird_eye,bird_div),2)
+fish_r<-round(cor(fish_eye,fish_div),2)
+mammal_r<-round(cor(mammal_eye,mammal_div),2)
+primate_r<-round(cor(primate_eye,primate_div),2)
+reptile_r<-round(cor(reptile_eye,reptile_div),2)
+rodent_r<-round(cor(rodent_eye,rodent_div),2)
+
+bird_rf<-paste("R²=",bird_r)
+fish_rf<-paste("R²=",fish_r)
+mammal_rf<-paste("R²=",mammal_r)
+primate_rf<-paste("R²=",primate_r)
+reptile_rf<-paste("R²=",reptile_r)
+rodent_rf<-paste("R²=",rodent_r)
+
 
 # x and y limitis
 x<-c(0,107)
@@ -117,12 +133,27 @@ y<-c(0,1108)
 
 # plot
 par(mfrow=c(2,3))
-#plot(amph_eye,amph_div,xlim=x,ylim=y,main="Amphibians",col="#588E47",lwd=2)
-#legend('topright', legend = "r=42", bty = 'n')
-plot(bird_eye,bird_div,xlim=x,ylim=y,main="Birds",col="#37642A")
-plot(fish_eye,fish_div,xlim=x,ylim=y,main="Eye axis vs. (eye axis)/(body weight)\n Fish",col="#FDE666",lwd=2)
-plot(mammal_eye,mammal_div,xlim=x,ylim=y,main="Mammal",col="#FFCB05")
-plot(primate_eye,primate_div,xlim=x,ylim=y,main="Primate",pch = "❤",col="#DE656B")
-plot(reptile_eye,reptile_div,xlim=x,ylim=y,main="Reptile",col="#C83B55",lwd=2)
-plot(rodent_eye,rodent_div,xlim=x,ylim=y,main="Rodent",col="#171411")
+plot(bird_eye,bird_div,xlim=x,ylim=y,main="Birds",ylab="Eye axis/weight",xlab="Eye axis",col="#37642A")
+abline(bird_line,lty=2,lwd=2,col="#8333a2")
+legend('topright', legend =bird_rf , bty = 'n')
+
+plot(fish_eye,fish_div,xlim=x,ylim=y,ylab="Eye axis/weight",xlab="Eye axis",main="Eye axis vs. (eye axis)/(body weight)\n Fish",col="#FDE666",lwd=2)
+abline(fish_line,lty=2,lwd=2,col="#8333a2")
+legend('topright', legend =fish_rf , bty = 'n')
+
+plot(mammal_eye,mammal_div,xlim=x,ylim=y,main="Mammal",ylab="Eye axis/weight",xlab="Eye axis",col="#FFCB05")
+abline(fish_line,lty=2,lwd=2,col="#8333a2")
+legend('topright', legend =mammal_rf , bty = 'n')
+
+plot(primate_eye,primate_div,xlim=x,ylim=y,main="Primate",ylab="Eye axis/weight",xlab="Eye axis",pch = "❤",col="#DE656B")
+abline(primate_line,lty=2,lwd=2,col="#8333a2")
+legend('topright', legend =primate_rf , bty = 'n')
+
+plot(reptile_eye,reptile_div,xlim=x,ylim=y,main="Reptile",ylab="Eye axis/weight",xlab="Eye axis",col="#C83B55",lwd=2)
+abline(reptile_line,lty=2,lwd=2,col="#8333a2")
+legend('topright', legend =reptile_rf , bty = 'n')
+
+plot(rodent_eye,rodent_div,xlim=x,ylim=y,main="Rodent",ylab="Eye axis/weight",xlab="Eye axis",col="#171411")
+abline(rodent_line,lty=2,lwd=2,col="#8333a2")
+legend('topright', legend =rodent_rf , bty = 'n')
 par(mfrow=c(1,1))
