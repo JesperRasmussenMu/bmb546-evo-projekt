a3b5064bdf419b6dfd066f956487472e118fb0f4
JesperRasmussenMu <jeras24@student.sdu.dk>
2025-03-26 03:57:14 +0100

    v.3

--- r code.R
+++ r code.R
@@ -6,8 +6,8 @@
 
 setwd("~/workdesk/bmb546 evo projekt")
 
-x<- read.csv("Howard.csv",header=T,sep="\t")
-y<- read.csv("Ritland_eyes_raw_FINAL.csv",header=T)
+x_stat<- read.csv("Howard.csv",header=T,sep="\t")
+y_stat<- read.csv("Ritland_eyes_raw_FINAL.csv",header=T)
 
 
 
@@ -15,7 +15,7 @@ y<- read.csv("Ritland_eyes_raw_FINAL.csv",header=T)
 names<-unique(x$Group)
 
 
-# Data untangling of 'x' dataset
+# Data untangling of 'x_stat' dataset
 DataPull<-function(dyr,navn,indsæt)
 {
   result<-c(0,0)
@@ -23,9 +23,9 @@ DataPull<-function(dyr,navn,indsæt)
   
   for (i in 1:333)
   {
-    if (x[i,dyr]==navn)
+    if (x_stat[i,dyr]==navn)
     { 
-      result[h]<-x[i,indsæt]
+      result[h]<-x_stat[i,indsæt]
       h<-h+1
     }
   }
@@ -67,9 +67,9 @@ h<-1
 
 for (i in 1:4782)
 {
-  if (y[i,40]=="Aquatic")
+  if (y_stat[i,40]=="Aquatic")
   { 
-    aquatic_eye[h]<-y[i,38]
+    aquatic_eye[h]<-y_stat[i,38]
     h<-h+1
   }
 }
@@ -81,9 +81,9 @@ h<-1
 
 for (i in 1:4782)
 {
-  if (y[i,40]=="Aquatic")
+  if (y_stat[i,40]=="Aquatic")
   { 
-    aquatic_weight[h]<-y[i,34]
+    aquatic_weight[h]<-y_stat[i,34]
     h<-h+1
   }
 }
@@ -93,22 +93,7 @@ for (i in 1:4782)
 #######################################################################
 #                         Data visolaztion                            #
 #######################################################################
-
-
-
-
-#par(mfrow=c(3,3))
-#plot(amph_weight,amph_eye)
-#plot(bird_weight,bird_eye)
-#plot(fish_weight,fish_eye)
-#plot(mammal_weight,mammal_eye)
-#plot(primate_weight,primate_eye)
-#plot(reptile_weight,reptile_eye)
-#plot(rodent_weight,rodent_eye)
-#par(mfrow=c(1,1))
-
-
-# Alternative plot
+# Y value
 amph_div<-amph_eye/amph_weight
 bird_div<-bird_eye/bird_weight
 fish_div<-fish_eye/fish_weight
@@ -116,14 +101,26 @@ mammal_div<-mammal_eye/mammal_weight
 primate_div<-primate_eye/primate_weight
 reptile_div<-reptile_eye/reptile_weight
 rodent_div<-rodent_eye/rodent_weight
-x<-c(0,107)
-y<-c(0,1000)
 
-par(mfrow=c(3,3))
-plot(amph_eye,amph_div,xlim=x,ylim=y,main="Amphibians",col="#588E47",lwd=2)
-legend('topright', legend = "r=42", bty = 'n')
-plot(bird_eye,bird_div,xlim=x,ylim=y,main="Eye axis vs. (eye axis)/(body weight)\n Birds",col="#37642A")
-plot(fish_eye,fish_div,xlim=x,ylim=y,main="Fish",col="#FDE666",lwd=2)
+# Line - not yet used - want to make R values
+amph_r<-lm(amph_div~amph_eye)
+bird_r<-lm(bird_div~bird_eye)
+fish_r<-lm(fish_div~fish_eye)
+mammal_r<-lm(mammal_div~mammal_eye)
+primate_r<-lm(primate_div~primate_eye)
+reptile_r<-lm(reptile_div~reptile_eye)
+rodent_r<-lm(rodent_div~rodent_eye)
+
+# x and y limitis
+x<-c(0,107)
+y<-c(0,1108)
+
+# plot
+par(mfrow=c(2,3))
+#plot(amph_eye,amph_div,xlim=x,ylim=y,main="Amphibians",col="#588E47",lwd=2)
+#legend('topright', legend = "r=42", bty = 'n')
+plot(bird_eye,bird_div,xlim=x,ylim=y,main="Birds",col="#37642A")
+plot(fish_eye,fish_div,xlim=x,ylim=y,main="Eye axis vs. (eye axis)/(body weight)\n Fish",col="#FDE666",lwd=2)
 plot(mammal_eye,mammal_div,xlim=x,ylim=y,main="Mammal",col="#FFCB05")
 plot(primate_eye,primate_div,xlim=x,ylim=y,main="Primate",pch = "❤",col="#DE656B")
 plot(reptile_eye,reptile_div,xlim=x,ylim=y,main="Reptile",col="#C83B55",lwd=2)
